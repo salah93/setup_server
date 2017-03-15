a928559d64cad042d2574792df3b315eb623cb84
salah ahmed <salah93@crosscompute.com>
2017-03-15 13:44:05 -0400

    add bootstrap steps in setup role

--- setup.sh
+++
@@ -1,7 +0,0 @@
-commands='sudo dnf install -y python; sudo dnf install -y python2-dnf'
-user='root'
-inventory_file='hosts';
-for ip in $(grep -v '\[.*\]' $inventory_file); do
-    echo "ssh $user@$ip $commands";
-    ssh $user@$ip $commands;
-done
