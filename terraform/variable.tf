08cc288649922a11432db2ba1bb1c354ca9c0327
salah ahmed <salahs.email@pm.me>
2020-03-15 10:47:21 -0400

    update variables

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -5,42 +5,22 @@ variable "do_token" {
 variable "ssh_keys" {
     description = "ssh key ids for digital ocean"
     type        = list
-    default    = ["5407023", "11430846", "19610406"]
 }
 
 variable "private_key" {
   description = "private key"
-  default     = "~/.ssh/id_rsa"
 }
 
 variable "remote_user" {
   description = "remote user"
-  default     = "salah"
 }
 
 variable "image" {
   description = "Image to use"
-  default     = "fedora-27-x64"
 }
 
 variable "size" {
   description = "Size of Droplet"
-  default     = "512mb"
-}
-
-variable "name" {
-  description = "Name of Droplet"
-  default     = "sandbox"
-}
-
-variable "site-size" {
-  description = "Size of Droplet"
-  default     = "1gb"
-}
-
-variable "site-name" {
-  description = "Name of Droplet"
-  default     = "site"
 }
 
 variable "region" {
