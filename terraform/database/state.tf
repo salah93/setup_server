9c1aea8ef18a9d52e9d686eeda22a2ff32d45dbd
Salah Ahmed <salahs.email@pm.me>
2020-03-22 23:52:53 -0400

    convert state to file from symbolic link

--- terraform/database/state.tf
+++ terraform/database/state.tf
@@ -1,1 +1,13 @@
-../state.tf
+provider "aws" {
+    region = "us-east-2"
+}
+
+terraform {
+    backend "s3" {
+        region         = "us-east-2"
+        bucket         = "salah-terraform-state"
+        dynamodb_table = "salah-terraform-locks"
+        key            = "state/database.tfstate"
+        encrypt        = true
+    }
+}
